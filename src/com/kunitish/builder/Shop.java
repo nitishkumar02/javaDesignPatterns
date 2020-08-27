05395c198be20fa1f91e096ff9bf920e5955691c
nk10008 <nkgupta3541@gmail.com>
2020-08-27 20:31:48 +0530

    using builder Pattern Builder

--- src/com/kunitish/builder/Shop.java
+++ src/com/kunitish/builder/Shop.java
@@ -7,8 +7,14 @@ public class Shop {
 		 * here we need to specify every argument to constructor, but what if we have to pass onle some
 		 * parameters
 		 */
-		Phone p1 = new Phone("samsung", 2,"Qulcoom",24,4500);
+		//Phone p1 = new Phone("samsung", 2,"Qulcoom",24,4500);
+		/*
+		 * here You can specify as much param u want to have the object
+		 */
+		Phone p1 = new PhoneBuilder().setOs("Andorid").setRam(2).getPhone();
 		System.out.println(p1);
+		
+		
 	}
 
 }
